cbab3f8647285f6c7c602252c1fe7b6523d62a2a
Habeeb24x7 <Habeeb24x7@users.noreply.github.com>
2014-10-24 11:40:41 +1100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,16 +1,5 @@
-    # /root/examples/motd.pp
+package { 'default-jdk' }
 
-    file {'habeeb':
-      ensure  => file,
-      path    => '/etc/habeeb',
-      mode    => 0644,
-      content => "This Learning Puppet VM's IP address is ${ipaddress}. It thinks its
-    hostname is ${fqdn}, but you might not be able to reach it there
-    from your host machine. It is running ${operatingsystem} ${operatingsystemrelease} and
-    Puppet ${puppetversion}.
-    Web console login:
-      URL: https://${ipaddress_eth0}
-      User: puppet@example.com
-      Password: learningpuppet
-    ",
-    }
+class {'solr::jetty':
+  zookeeper_hosts        => ["172.31.0.111:2181", "172.31.0.112:2181", 172.31.0.113:2181]
+}
